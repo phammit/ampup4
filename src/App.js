b08d4b240de85819f06cfee87c06b71a9b50073e
minh pham <phammit@gmail.com>
2021-01-08 01:01:07 -0600

    +graphql code update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,62 @@
-
+import React, { useState, useEfect, useEffect } from 'react';
 import './App.css';
+import { API } from 'aws-amplify';
+import { listItems } from './graphql/queries';
+import { createItem as createItemMutation, deleteItem as deleteItemMutation} from './graphql/mutations';
 
+const initialFormState = { title: '', description: ''};
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listItems });
+    setItems(apiData.data.listItems.items);
+  }
+
+  async function createItem() {
+    if (!formData.title || !formData.description) return;
+    await API.graphql({ query: createItemMutation, variables: { input: formData } });
+    setItems([ ...items, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteItem({ id }) {
+    const newItemsArray = items.filter(item => item.id !== id);
+    setItems(newItemsArray);
+    await API.graphql({ query: deleteItemMutation, variable: { input: { id } }});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Welcome to AmpUp4!!</p>
-        <p>Testing Authentication</p>
-        <p>lets try some new stuff</p>
-      </header>
+      <h1>Catalog of Items</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'title': e.target.value})}
+        placeholder="Item title"
+        value={formData.title}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Item description"
+        value={formData.description}
+      />
+      <button onClick={createItem}>Create Item</button>
+      <div style={{marginBottom: 30}}>
+        {
+          items.map(item => (
+            <div key={item.id || item.title}>
+              <h2>{item.title}</h2>
+              <p>{item.description}</p>
+              <button onClick={() => deleteItem(item)}>Delete Item</button>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
